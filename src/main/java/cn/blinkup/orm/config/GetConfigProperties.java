ee1fd5b3ab58d6cb8e38bea0c80d9aacd0ee925d
zhangguosheng <919323345@qq.com>
2022-02-15 21:28:18 +0800

    [Feature]引入jsp相关pom

--- src/main/java/cn/blinkup/orm/config/GetConfigProperties.java
+++ src/main/java/cn/blinkup/orm/config/GetConfigProperties.java
@@ -1,8 +1,15 @@
 package cn.blinkup.orm.config;
 
+import org.yaml.snakeyaml.Yaml;
+
+import java.util.List;
+
 /**
  * @author zhangguosheng
  */
 public class GetConfigProperties {
+    Yaml yaml = new Yaml();
+    List<String> ret = yaml.load(this.getClass().getClassLoader().getResourceAsStream("application.yaml"));
     
+
 }
