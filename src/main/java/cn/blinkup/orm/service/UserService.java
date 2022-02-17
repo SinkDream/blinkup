e940463d62eccb820fb2cfa7f0813fe73dfa38fa
joe <919323345@qq.com>
2022-02-17 14:02:45 +0800

    [Feature]用户服务类添加方法

---
+++ src/main/java/cn/blinkup/orm/service/UserService.java
@@ -0,0 +1,30 @@
+package cn.blinkup.orm.service;
+
+import java.util.Map;
+
+/**
+ * @author joe
+ * 用户服务
+ */
+public class UserService {
+
+    public void createUser(Map<String, Object> userParamMap){
+        //todo
+    }
+
+    public void deleteUser(long userId){
+        //todo
+    }
+
+    public void updateUser(Map<String, Object> userParamMap){
+        //todo
+    }
+
+    public void getUserDetail(long userId){
+        //todo
+    }
+
+    public void getUserList(Map<String, Object> listParamMap){
+        //todo
+    }
+}
