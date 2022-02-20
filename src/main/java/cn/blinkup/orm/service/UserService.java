9fd1cbc46dece14485bd217eb97879078e468048
zhangguosheng <919323345@qq.com>
2022-02-20 11:06:24 +0800

    [Feature]在UserService中操作数据库

--- src/main/java/cn/blinkup/orm/service/UserService.java
+++ src/main/java/cn/blinkup/orm/service/UserService.java
@@ -1,12 +1,28 @@
 package cn.blinkup.orm.service;
 
+import cn.blinkup.orm.utils.PropsUtil;
+
 import java.util.Map;
+import java.util.Properties;
 
 /**
  * @author joe
  * 用户服务
  */
 public class UserService {
+
+    private static final String DRIVER;
+    private static final String URL;
+    private static final String USERNAME;
+    private static final String PASSWORD;
+
+    static {
+        Properties conf = PropsUtil.loadProps("config.properties");
+        DRIVER = conf.getProperty("jdbc.driver");
+        URL = conf.getProperty("jdbc.url");
+        USERNAME = conf.getProperty("jdbc.username");
+        PASSWORD = conf.getProperty("jdbc.password");
+    }
 
     public void createUser(Map<String, Object> userParamMap){
         //todo
