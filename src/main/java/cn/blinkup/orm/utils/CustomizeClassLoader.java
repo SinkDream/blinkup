fc77fa51af04b07c3f248ca768eb31b153a18ca4
zhangguosheng <919323345@qq.com>
2022-02-23 20:49:56 +0800

    [Feature]完成自定义类加载器的部分功能

--- src/main/java/cn/blinkup/orm/utils/CustomizeClassLoader.java
+++ src/main/java/cn/blinkup/orm/utils/CustomizeClassLoader.java
@@ -1,9 +1,19 @@
 package cn.blinkup.orm.utils;
 
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.File;
+import java.io.FileFilter;
+import java.io.IOException;
+import java.net.JarURLConnection;
+import java.net.URL;
+import java.util.Enumeration;
+import java.util.HashSet;
 import java.util.Set;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
 
 /**
  * @author zhangguosheng
@@ -44,7 +54,62 @@ public final class CustomizeClassLoader {
      * @return
      */
     public static Set<Class<?>> getClassSet(String packageName){
-        //todo
+        Set<Class<?>> classSet = new HashSet<>();
+        try{
+            Enumeration<URL> urls = getClassLoader().getResources(packageName.replace(".", "/"));
+            while (urls.hasMoreElements()){
+                URL url = urls.nextElement();
+                if(null != url){
+                    String protocol = url.getProtocol();
+                    if(protocol.equals("file")){
+                        //过滤空格
+                        String packagePath = url.getPath().replaceAll("%20", "");
+                        addClass(classSet, packagePath, packageName);
+                    } else if(protocol.equals("jar")){
+                        JarURLConnection jarURLConnection = (JarURLConnection) url.openConnection();
+                        if(null != jarURLConnection){
+                            JarFile jarFile = jarURLConnection.getJarFile();
+                            if(null != jarFile){
+                                Enumeration<JarEntry> jarEntries = jarFile.entries();
+                                while (jarEntries.hasMoreElements()){
+                                    JarEntry jarEntry = jarEntries.nextElement();
+                                    String jarEntryName = jarEntry.getName();
+                                    if(jarEntryName.endsWith(".class")){
+                                        String className = jarEntryName.substring(0, jarEntryName.lastIndexOf(".")).replaceAll("/",  ".");
+                                        doAddClass(classSet, className);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return null;
+    }
+
+    private static void addClass(Set<Class<?>> classSet, String packagePath, String packageName){
+        File[] files = new File(packagePath).listFiles(new FileFilter(){
+            @Override
+            public boolean accept(File pathname) {
+                return (pathname.isFile() && pathname.getName().endsWith(".class")) || pathname.isDirectory();
+            }
+        });
+        for (File file : files) {
+            String fileName = file.getName();
+            if(file.isFile()){
+                String className = fileName.substring(0, fileName.lastIndexOf("."));
+                if(StringUtils.isNotEmpty(packageName)){
+                    //todo
+                }
+            }
+        }
+    }
+
+    private static void doAddClass(Set<Class<?>> classSet, String className){
+        Class<?> clazz = loadClass(className, false);
+        classSet.add(clazz);
     }
 }
