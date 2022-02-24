22926caa47287505d4d1ff735e374a7e6de825b0
zhangguosheng <919323345@qq.com>
2022-02-24 21:57:33 +0800

    [Feature]获取请求路径的方法注解

---
+++ src/main/java/cn/blinkup/orm/annotation/Zgs.java
@@ -0,0 +1,17 @@
+package cn.blinkup.orm.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author zhangguosheng
+ * Action方法注解
+ */
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Zgs {
+    //请求路径
+    String value();
+}
