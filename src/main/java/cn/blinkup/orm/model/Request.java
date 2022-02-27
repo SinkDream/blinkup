9deafe8a28120e1a7704866020c681fdaa0e4ae7
zhangguosheng <919323345@qq.com>
2022-02-27 12:27:23 +0800

    [Feature]添加请求request实体

---
+++ src/main/java/cn/blinkup/orm/model/Request.java
@@ -0,0 +1,52 @@
+package cn.blinkup.orm.model;
+
+import java.util.Objects;
+
+/**
+ * @author zhangguosheng
+ */
+public class Request {
+
+    /**
+     * 请求方法
+     */
+    private String method;
+
+    /**
+     * 请求路径
+     */
+    private String path;
+
+    @Override
+    public boolean equals(Object o) {
+        if(this == o){
+             return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Request request = (Request) o;
+        return Objects.equals(method, request.method) && Objects.equals(path, request.path);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(method, path);
+    }
+
+    public String getMethod() {
+        return method;
+    }
+
+    public void setMethod(String method) {
+        this.method = method;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+}
