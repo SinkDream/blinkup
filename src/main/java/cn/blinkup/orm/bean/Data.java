44a2d3c5b7c415e78157c227266e5a5f2cac8234
zhangguosheng <919323345@qq.com>
2022-02-28 21:24:55 +0800

    [Feature]添加数据对象实体

---
+++ src/main/java/cn/blinkup/orm/bean/Data.java
@@ -0,0 +1,16 @@
+package cn.blinkup.orm.bean;
+
+/**
+ * @author zhangguosheng
+ */
+public class Data {
+    private Object model;
+
+    public Data(Object model) {
+        this.model = model;
+    }
+
+    public Object getModel() {
+        return model;
+    }
+}
