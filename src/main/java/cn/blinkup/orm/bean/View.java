55cc6ffbbfcecc5da42469b5292da24d3b84170f
zhangguosheng <919323345@qq.com>
2022-02-28 21:23:57 +0800

    [Feature]添加视图对象实体

---
+++ src/main/java/cn/blinkup/orm/bean/View.java
@@ -0,0 +1,32 @@
+package cn.blinkup.orm.bean;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author zhangguosheng
+ * 视图对象
+ */
+public class View {
+    private String path;
+
+    private Map<String, Object> model;
+
+    public View(String path){
+        this.path = path;
+        model = new HashMap<>();
+    }
+
+    public View addModel(String key, Object value){
+        model.put(key, value);
+        return this;
+    }
+
+    public String getPath(){
+        return path;
+    }
+
+    public Map<String, Object> getModel(){
+        return model;
+    }
+}
